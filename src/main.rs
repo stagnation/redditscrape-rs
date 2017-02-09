3ed80e1d86175178e2965f3f6a8cbbfc0a812e64
Nils Wireklint <nils@wireklint.se>
2017-02-09 20:54:58 +0100

    fixed some issues with path_segments and bump URL lib

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@ extern crate serde_json;
 extern crate curl;
 
 use std::fs::File;
+use std::path::PathBuf;
 use std::io::prelude::*;
 use std::io::Error;
 use std::io::BufReader;
+use std::collections::VecDeque;
 
 use url::Url;
 use curl::easy::Easy;
@@ -19,19 +21,100 @@ struct RedditEntry {
     comments: Option<u64>,
 }
 
-struct Cache {}
+use std::collections::HashMap;
+struct Cache {
+    storage: HashMap<Url, PathBuf>,
+    directory: PathBuf,
+
+}
 
 impl Cache {
-    fn try_to_get(&self, link: &Url) -> Option<Json> {
-        unimplemented!()
+    fn try_to_get(&self, key: &Url) -> Option<Json> {
+        self.storage.get(key).and_then(load_json_file)
     }
 
-    fn store(&mut self, data: &String) -> Result<(), Error> {
+    fn store(&mut self, key: Url, data: &Json) -> Result<(), Error> {
+        let filename = filename_from_link(&key, &self.directory);
+        match save_json_file(&filename, &data) {
+            // TODO(nils): and_then?
+            Ok(()) => {
+                self.storage.insert(key, filename);
+                Ok(())
+            },
+            Err(e) => Err(e)
+        }
+    }
+
+    pub fn load_cache(path: &PathBuf) -> Cache {
         unimplemented!()
     }
 }
 
 type Json = String;
+
+fn load_json_file<T>(path: T) -> Option<Json>
+where T: AsRef<std::path::Path> {
+    let mut file = File::open(path).expect("could not open file");
+    let mut json: Json = String::new();
+    let read_result = file.read_to_string(&mut json);
+
+    match read_result {
+        Ok(_) => Some(json),
+        _ => None,
+    }
+}
+
+fn save_json_file<T>(path: T, json: &Json) -> Result<(), Error>
+where T: AsRef<std::path::Path> {
+    let mut file = File::create(path).expect("could not open file");
+    file.write_all(json.as_bytes())
+}
+
+/// canonical storage form is : [id_]name
+/// with optional id, using reddits internal id for threads
+/// and name from the post title
+fn filename_from_link<T>(link: &Url, base: T) -> PathBuf
+where T: AsRef<std::path::Path> {
+    let base :&std::path::Path = base.as_ref();
+
+    let path_vec = link.path_segments().map(|c| c.collect::<Vec<_>>());
+    let mut path_vec = path_vec.expect("could not extract path segments");
+    path_vec.retain(|elem| elem.len() > 1);
+    let path_vec = path_vec;
+
+    assert!(path_vec.len() >= 2);
+    let id = path_vec[path_vec.len() - 2];
+    let title = path_vec[path_vec.len() - 1];
+    let filename = format!("{}_{}.json", id, title);
+
+    base.join(filename)
+}
+/* {
+    // want the last two elements
+    let RENAME = link.clone();
+    // FIXME(nils): path_segments may not work as intended
+    // FIXME(nils): and only return the full string as one segment
+    let RENAME = RENAME.path_segments();
+
+    let mut queue = VecDeque::with_capacity(2);
+    println!(":{:?}", link);
+    for seg in RENAME {
+        println!("-{:?}", seg);
+        if queue.len() == 2 {
+            queue.pop_front();
+        }
+        queue.push_back(seg.clone().collect::<String>());
+    }
+    //
+    // now queue should be populated with the last two elements in order
+    let id = queue.get(0).expect("error no information in path").clone();
+    let title = queue.get(1).expect("error no information in path").clone();
+    let filename = format!("{}_{}.json", id, title);
+
+    let base = base.clone();
+    base.join(filename)
+}
+*/
 
 macro_rules! unwrap_or_skip {
     ($result:ident, $message:expr) => {
@@ -78,8 +161,8 @@ fn parse_reddit_json(json: Json) -> RedditEntry {
         url:       value_to_string(deref.find("url")),
         title:     value_to_string(deref.find("title")),
         subreddit: value_to_string(deref.find("subreddit")),
-        votes:     deref.find("score")       .map(|x| x.as_u64().unwrap()),
-        comments:  deref.find("num_comments").map(|x| x.as_u64().unwrap()),
+        votes:     deref.find("score")       .and_then(|x| x.as_u64()),
+        comments:  deref.find("num_comments").and_then(|x| x.as_u64()),
     }
 }
 
@@ -90,7 +173,7 @@ fn get_jsons(links: Vec<Url>, cache: &mut Cache) -> Vec<Json> {
             Some(json) => json,
             None => {
                 let json = download_json(link.clone());
-                cache.store(&json).expect("could not store in cache");
+                cache.store(link, &json).expect("could not store in cache");
                 json
             },
         };
@@ -99,11 +182,9 @@ fn get_jsons(links: Vec<Url>, cache: &mut Cache) -> Vec<Json> {
     jsons
 }
 
-fn cached_json(link: &Url, cache: &Cache) -> Option<Json> {
-    unimplemented!()
-}
-
+// NB(nils): keep in mind reddit's two second rule
 fn download_json(link: Url) -> Json {
+    // NB(nils): keep in mind reddit's two second rule
     let link = ensure_json_link(link);
 
     let mut handle = Easy::new();
@@ -125,6 +206,8 @@ fn download_json(link: Url) -> Json {
 }
 
 fn ensure_json_link(link: Url) -> Url {
+    // FIXME(nils): path_segments may not work as intended
+    // FIXME(nils): and only return the full string as one segment
     let last_interim = link.clone();
     let last_interim = last_interim.path_segments();
     let last = last_interim.expect("invalid url").last().expect("empty path segments");
@@ -138,6 +221,7 @@ fn ensure_json_link(link: Url) -> Url {
 }
 
 fn main() {
+    // NB(nils): provide cache directory
     let input_file = File::open("input.txt");
     let input_file = match input_file {
         Ok(f) => f,
@@ -157,14 +241,12 @@ mod test {
     use super::Json;
     use super::*;
     use std::fs::File;
+    use std::path::PathBuf;
 
     #[test]
     fn test_reddit_from_json() {
         let test_filename = "reddit.json";
-        let mut test_file = File::open(test_filename).expect("could not open file");
-        let mut json: Json = String::new();
-        let read_result = test_file.read_to_string(&mut json);
-        assert!(read_result.is_ok());
+        let json = load_json_file(test_filename).unwrap();
 
         let result = parse_reddit_json(json);
         let expected = RedditEntry{
@@ -201,6 +283,27 @@ mod test {
         let url = Url::parse("http://aelv.se/spill/ul/test_json.json").expect("could not parse url");
 
         assert_eq!(ensure_json_link(url.clone()), url);
+    }
+
+    #[test]
+    fn test_dependency_path_segment() {
+        assert_eq!(Url::parse("https://github.com/rust-lang/rust/issues").expect("could not parse url")
+                   .path_segments().map(|c| c.collect::<Vec<_>>()),
+                   Some(vec!["rust-lang", "rust", "issues"]));
+
+        let url = Url::parse("https://www.reddit.com/r/BlackMetal/comments/5elhkp/spectral_lore_cosmic_significance").unwrap();
+        assert_eq!(url.path_segments().map(|c| c.collect::<Vec<_>>()),
+                   Some(vec!["r", "BlackMetal", "comments",
+                             "5elhkp", "spectral_lore_cosmic_significance"]));
+    }
+
+    #[test]
+    fn test_filename_from_link() {
+        let url = Url::parse("https://www.reddit.com/r/BlackMetal/comments/5elhkp/spectral_lore_cosmic_significance/").unwrap();
+        let directory = "/tmp";
+        let expected = PathBuf::from("/tmp/5elhkp_spectral_lore_cosmic_significance.json");
+
+        assert_eq!(filename_from_link(&url, directory), expected);
     }
 
     #[test]
